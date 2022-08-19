15cdfcc6706b83e34f63c1c8dd0d51ed1fda36ee
윤관 <blackgar@naver.com>
2022-08-19 15:57:39 +0900

    Fix Edit button and Upload Cancel button

--- src/pages/Todo/TodoListItem.tsx
+++ src/pages/Todo/TodoListItem.tsx
@@ -110,22 +110,36 @@ function Item({ task, onRemove, onTodoChange }: ITodoItem) {
         </TodoWrapper>
         <ButtonWrapper>
           {active ? (
-            <EditButton onClick={onEdit}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                style={{ width: "20px", height: "20px" }}
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </EditButton>
+            <>
+              <EditButton onClick={onEdit}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ width: "20px", height: "20px" }}
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </EditButton>
+              <RemoveButton onClick={() => setActive(false)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  style={{ width: "20px", height: "20px" }}
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </RemoveButton>
+            </>
           ) : (
             <>
               <EditButton onClick={askEdit}>
